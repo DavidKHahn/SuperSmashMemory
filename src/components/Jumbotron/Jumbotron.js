b308b7124b74184c30187e705c839983719ecb6f
David <dhahn123@gmail.com>
2018-04-16 23:27:38 -0700

    v.1 files

---
+++ src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Jumbotron.css";
+
+
+const Jumbotron = () => (
+<div className="jumbotron jumbotron-fluid">
+  <div className="container text-center">
+    <h1 className="display-4">Super Smash Bros Memory Game!</h1>
+    <p className="lead">Click on any character only once to earn points!</p>
+  </div>
+</div>
+);
+
+export default Jumbotron;
